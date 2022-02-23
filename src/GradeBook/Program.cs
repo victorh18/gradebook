f5de63afc7afd8350136a8199fa7ef4bf88e26b7
Victor de la Rosa <videlarosa@humano.com.do>
2022-02-23 12:01:57 -0400

    finishing second chapter

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace GradeBook {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Executing Grading App...");
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]}");
+            } else {
+                Console.WriteLine("Hello User!");
+            }
+        }
+    }
+}
