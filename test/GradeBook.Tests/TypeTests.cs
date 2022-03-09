0dd5ade7789add8817c1f3b4d9b0e5653a7f1dd0
Victor de la Rosa <videlarosa@humano.com.do>
2022-03-09 12:31:19 -0400

    understanding parameter passing differences

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -7,6 +7,58 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
+        public void ByValueCannotSwapReferences()
+        {
+            /*
+            *   This shows that passing values by value in a method just copies whatever value is inside the outside variable and 
+            *   pastes it into a new one. Since there is a new object reference in a new variable, any new changes get applied to
+            *   this variable that already has a memory pointer. Since the changes get applied to the value in the inside variable,
+            *   the outside variable does not change (the change is not forwarded)
+            */
+            const string BOOK_1 = "Book 1";
+            const string BOOK_2 = "Book 2";
+            // Given
+            var book1 = new Book(BOOK_1);
+            var book2 = new Book(BOOK_2);
+            // When
+            SwapReferences(book1, book2);
+            // Then
+            Assert.Equal(BOOK_1, book1.Name);
+            Assert.Equal(BOOK_2, book2.Name);
+        }
+        [Fact]
+        public void ByRefCanSwapReferences()
+        {
+            /*
+            *   This shows that passing values by reference to a method gives it the power to modify the outside variable,
+            *   in this case to change where the outside reference is pointing to, because since inside the variable we are 
+            *   pointing to the outside variable, any changes will be forwarded to the outside variable.
+            */
+            const string BOOK_1 = "Book 1";
+            const string BOOK_2 = "Book 2";
+            // Given
+            var book1 = new Book(BOOK_1);
+            var book2 = new Book(BOOK_2);
+            // When
+            SwapReferences(ref book1, ref book2);
+            // Then
+            Assert.Equal(BOOK_1, book2.Name);
+            Assert.Equal(BOOK_2, book1.Name);
+        }
+
+        public void SwapReferences(Book book1, Book book2) {
+            Book tempBook = book1;
+            book1 = book2;
+            book2 = tempBook;
+        }
+
+        public void SwapReferences(ref Book book1, ref Book book2) {
+            Book tempBook = book1;
+            book1 = book2;
+            book2 = tempBook;
+        }
+
+        [Fact]
         public void TwoVariablesHoldSameInstanceButThenGetNewInstancesEach()
         {
             var book1 = new Book("Book 1");
